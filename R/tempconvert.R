1635a6397a63d593f0c30a911f28cdec75b3eba4
winstonsieck <sieck@globalcognition.org>
2024-07-21 21:11:51 -0400

    Added documentation to function

--- R/tempconvert.R
+++ R/tempconvert.R
@@ -1,4 +1,13 @@
-
+#' Convert from Fahrenheit to Celsius
+#'
+#' @param f_temp The temperature in Fahrenheit
+#'
+#' @return A numeric value
+#' @export
+#'
+#' @examples
+#' f_temp <- 32
+#' f_to_c(f_temp)
 f_to_c <- function(f_temp) {
   round((f_temp - 32) * 5/9,1)
 }
